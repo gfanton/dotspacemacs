118fa862caf262228737d28d32518b7e8eee641f
gfanton <guilhem.fanton@gmail.com>
2016-10-03 15:02:20 +0200

    MOVE private file load into main config

--- init.el
+++ init.el
@@ -2,24 +2,23 @@
 
 (let* ((dotspacemacs-basedir (file-name-directory (or load-file-name buffer-file-name)))
 
-       ;; main file
+       ;; Main file.
        (dotspacemacs-config-file (concat dotspacemacs-basedir "gfanton.org"))
 
-       ;; custom file
+       ;; Custom file.
        (dotspacemacs-custom-file (concat dotspacemacs-basedir "custom.el"))
 
-       ;; private file
+       ;; Private file
        (dotspacemacs-private-file (concat dotspacemacs-basedir "private.el")))
 
   (unless (file-exists-p dotspacemacs-custom-file)
     (write-region "" nil dotspacemacs-custom-file))
-  (setq custom-file dotspacemacs-custom-file)
 
   (unless (file-exists-p dotspacemacs-private-file)
     (write-region "" nil dotspacemacs-private-file))
 
-  (org-babel-load-file dotspacemacs-config-file)
+  (setq custom-file dotspacemacs-custom-file)
 
-  (load dotspacemacs-custom-file)
-  (load dotspacemacs-private-file))
+  (org-babel-load-file dotspacemacs-config-file) 
+  (load dotspacemacs-custom-file))
 
